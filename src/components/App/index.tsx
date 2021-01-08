4abfdbb4d9eef636f42d59690b0dca80fe0a75ad
Chintana Meegamarachchi <chintana@appsolve.com.au>
2021-01-08 20:17:08 +0800

    Display title text using MUI

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { Typography } from "@material-ui/core";
+import React from "react";
+
+const ApplicationTitle = ({ text }: { text: string }) => (
+  <Typography variant="h1" component="h2">
+    {text}
+  </Typography>
+);
 
 const App = () => {
-	return (
-		<div>
-			Hi	
-		</div>
-	)
-}
+  return <ApplicationTitle text="Application title" />;
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import App from './components/App';
 import './index.css';
 
 import "typeface-roboto";
 
+const defaultTheme = createMuiTheme();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+	<ThemeProvider theme={defaultTheme}>  
+    	<App />
+	</ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
