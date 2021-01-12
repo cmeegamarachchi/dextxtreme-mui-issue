4eeb188e526546e22334c2a2c5833aa1569e9a72
Chintana Meegamarachchi <chintana@appsolve.com.au>
2021-01-12 08:32:43 +0800

    Imports dx css

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,6 +2,8 @@ import { Typography } from "@material-ui/core";
 import { Button } from "devextreme-react";
 import React from "react";
 
+import "./../../css/dx.material.dx-mui-blue-light.css"
+
 const ApplicationTitle = ({ text }: { text: string }) => (
   <Typography variant="h1" component="h2">
     {text}
